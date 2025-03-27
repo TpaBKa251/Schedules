00416cd8aa679d9972f634a01862105786bbfb49
darmren <dem24@tpu.ru>
2025-03-28 01:20:12 +0700

    HOSTEL-76 реализован ResponsibleSetDto

---
+++ src/main/java/ru/tpu/hostel/schedules/dto/request/ResponsibleSetDto.java
@@ -0,0 +1,19 @@
+package ru.tpu.hostel.schedules.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import ru.tpu.hostel.schedules.enums.EventType;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record ResponsibleSetDto(
+        @NotNull(message = "Дата назначения ответственного не может быть пустой")
+        LocalDate date,
+
+        @NotNull(message = "Тип ответственного не может быть пустым")
+        EventType type,
+
+        @NotNull(message = "ID ответственного не может быть пустым")
+        UUID user
+) {
+}
